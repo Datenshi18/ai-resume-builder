383d86bf38ec6ab725209652332c5b5d20b6e23d
Datenshi18 <155434122+Datenshi18@users.noreply.github.com>
2026-02-14 17:55:06 +0530

    Initial backend setup with MongoDB connection

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+//Mongodb connection 
+const mongoose = require('mongoose');
+
+const connectDb = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("MongoDb connected")
+    }
+    catch(error){
+        console.error("Mongodb connection error",error.message);
+        process.exit(1);// “If the database connection fails, print the error and stop the server with an error status.”
+    }
+}
+module.exports= connectDb;
+console.log("***************************************")
+console.log(process.env.MONGO_URL)
